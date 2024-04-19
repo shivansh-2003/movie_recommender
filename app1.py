96313eab48b6ddfc26307c15327bdf64e0eef9ad
Shivansh mahajan <144326194+shivansh-2003@users.noreply.github.com>
2024-04-19 12:06:31 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -3,25 +3,17 @@ import pickle
 import pandas as pd
 import requests
 
-def fetch_poster(movie_id):
-    url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id)
-    data = requests.get(url)
-    data = data.json()
-    poster_path = data['poster_path']
-    full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
-    return full_path
+
 
 def recommend(movie):
     index = movies[movies['title'] == movie].index[0]
     distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
     recommended_movie_names = []
-    recommended_movie_posters = []
     for i in distances[1:6]:
         movie_id = movies.iloc[i[0]].movie_id
-        recommended_movie_posters.append(fetch_poster(movie_id))
         recommended_movie_names.append(movies.iloc[i[0]].title)
 
-    return recommended_movie_names,recommended_movie_posters
+    return recommended_movie_names
 
 # Load the movie_list.pkl file
 movie_list_path = '/Users/shivanshmahajan/Desktop/DataScinece/Machine Learning/Projects/movie_recommender/movie_list.pkl'
